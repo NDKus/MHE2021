950dd6018fb054667d3bd1c7dfa893ae66db0f67
Przemysław Scharmach <s17279@pjwstk.edu.pl>
2021-05-22 14:23:04 +0200

    Modyfikacja skryptu eksperymentu

--- exp.sh
+++ exp.sh
@@ -1,6 +1,6 @@
 echo "simAnnealing";
-python ./minimumVertexCover_v3.py -simAnnealing > simAnnealing.txt
+python ./minimumVertexCover_v3.py -simAnnealing > simAnnealing.csv
 echo "hillClimbingDeterministic";
-python ./minimumVertexCover_v3.py -hillClimbingDeterministic > hillClimbingDeterministic.txt
+python ./minimumVertexCover_v3.py -hillClimbingDeterministic > hillClimbingDeterministic.csv
 echo "hillClimbingRandomized";
-python ./minimumVertexCover_v3.py -hillClimbingRandomized > hillClimbingRandomized.txt
+python ./minimumVertexCover_v3.py -hillClimbingRandomized > hillClimbingRandomized.csv
